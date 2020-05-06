9e321b7214f3a0e9ea5e0de5c1eabc4ffbcb9623
Neumann <Neumann Hon>
2020-05-06 19:15:45 -0400

    Fix indentation, add usage print

--- src/main.py
+++ src/main.py
@@ -15,27 +15,33 @@ def getOptions():
 
 
 def findJsonFiles(path):
-  onlyjson = [join(path, f) for f in listdir(path) if (isfile(join(path, f)) and (".json" in f))]
-  json_dicts = []
-  for js in onlyjson:
-    f = open(js, 'r')
-    jsonDicts.append(json.laod(f))
-    f.close()
-  return json_dicts
-  
+    onlyjson = [join(path, f) for f in listdir(path) if (isfile(join(path, f)) and (".json" in f))]
+    json_dicts = []
+    for js in onlyjson:
+        f = open(js, 'r')
+        jsonDicts.append(json.laod(f))
+        f.close()
+    return json_dicts
+
+
+def printUsage():
+    print("")
+    print("Usage:")
+    print("  python3 main.py --logs <path/to/dir/with/chatlogs --output <path/to/output/file>")
 
 
 if __name__ == "__main__":
-  cl_options = getOptions()
-  
-  # Make sure chatlogs exist
-  if (not cl_options.logs):
-    print("Must specify path to Facebook Chatlogs")
-    sys.exit(1)
-  
-  path = cl_options.logs
-  # Verify that path is valid
-  if (not isdir(path)):
-    print("Invalid path to Facebook Chatlogs")
-    sys.exit(1)
-    
+    cl_options = getOptions()
+
+    # Make sure chatlogs exist
+    if (not cl_options.logs):
+        print("Must specify path to Facebook Chatlogs")
+        printUsage()
+        sys.exit(1)
+
+    path = cl_options.logs
+    # Verify that path is valid
+    if (not isdir(path)):
+        print("Invalid path to Facebook Chatlogs")
+        printUsage()
+        sys.exit(1)
